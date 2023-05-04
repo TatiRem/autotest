a519c59107e7b5118490c3ca5a82c33c2269a311
tati <remigi_tati@mail.ru>
2023-05-04 23:12:36 +0300

    fix change to asserts

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,11 +1,9 @@
 import { capitalize } from '../src/capitalize.js';
 
-if (capitalize('hello') !== 'Hello') {
-  throw new Error('Функция работает неверно!');
-}
+import { strict as assert } from 'node:assert';
 
-if (capitalize('') !== '') {
-  throw new Error('Функция работает неверно!');
-}
+assert.equal(capitalize('hello'), 'Hello');
+
+assert.equal(capitalize(''), '');
 
 console.log('Все тесты пройдены!');
